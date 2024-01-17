0c113f12ef91df718d921071da2fc010aee0efe3
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-17 08:38:25 -0800

    Played with sending move up and then removed

--- client.js
+++ client.js
@@ -31,3 +31,10 @@ const connect = function () {
 module.exports = {
   connect
 }
+
+// All move commands
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
