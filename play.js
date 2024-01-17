b98453ebd92a041e231d381c9599ec9b53a8cf99
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-17 08:17:45 -0800

    Seperate clint connect function into its own file

--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "172.21.185.52",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
-
-
-
+const { connect } = require("./client");
 console.log("Connecting ...");
 connect();
