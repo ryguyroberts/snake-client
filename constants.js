1c27b59a20026ba5ae798490a2beca0593c284b9
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-17 10:14:26 -0800

    Created seperate file for constants modified client to take

---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = "172.21.185.52";
+const PORT = "50541";
+
+module.exports = {
+  IP,
+  PORT,
+};
--- input.js
+++ input.js
@@ -40,11 +40,3 @@ const setupInput = function(conn) {
 module.exports = {
   setupInput
 };
-
-
-// All move commands
-
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
