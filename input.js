2588a9718aaf0b96046946037f42943fe6825596
Ryan Robertson <ryguyrobertson@gmail.com>
2024-01-17 08:58:23 -0800

    Seperated user input function into its own file

---
+++ input.js
@@ -0,0 +1,20 @@
+//How we handle user input 
+const handleUserInput = (input) => {
+  // exit on Ctrl + C
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+}
--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const { connect } = require("./client");
-
-//How we handle user input 
-const handleUserInput = (input) => {
-  // exit on Ctrl + C
-  if (input === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
